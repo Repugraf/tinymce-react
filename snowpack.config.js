0878a50944b0067170d4ebc048374861f125cd0f
Serhii Mamedov <bboyvivus@gmail.com>
2020-12-03 18:35:57 +0200

    Setup snopack

---
+++ snowpack.config.js
@@ -0,0 +1,35 @@
+/** @type {import("snowpack").SnowpackUserConfig} */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+    "@snowpack/plugin-sass",
+    ["@snowpack/plugin-run-script", {
+      "cmd": "eslint --fix .",
+      "watch": "watch \"$1\" src"
+    }]
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  }
+};
